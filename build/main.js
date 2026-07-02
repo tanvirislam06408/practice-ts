ef9cb2c48ba90416440b1cd9793d8b83e7df7e8f
tanvir <mstanvirislam05@gmail.com>
2026-07-02 09:51:35 +0600

    Refactor main.ts to implement TypeScript types and improve function definitions

--- build/main.js
+++ build/main.js
@@ -1,7 +1,34 @@
-let userName = "Tanvir-Hassan";
-let a = 5;
-let b = '2';
-let c = 12;
-console.log(a / b);
+// let myName: string;
+// let meaningOfLife: number;
+// let isLoading: boolean;
+// let album: number | string;
+// let re: RegExp = /\w+/g
+let newUser = 'dave';
+const sum = (a, b) => {
+    return a + b;
+};
+const logMeg = (meg) => {
+    console.log(meg);
+};
+logMeg('hello !');
+logMeg(sum(4, 3));
+logMeg(sum(10, 10));
+let multi = (a, b) => {
+    return a * b;
+};
+// optional parameters
+const addAll = (a, b, c = 0) => {
+    // if (typeof (c) !== 'undefined') {
+    //     return a + b + c
+    // }
+    return a + b + c;
+};
+logMeg(addAll(2, 2, 5));
+logMeg(addAll(2, 2));
+// rest parameters
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMeg(total(1, 2, 3, 5, 74));
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,99 @@
-let userName="Tanvir-Hassan"
+// let myName: string;
+// let meaningOfLife: number;
+// let isLoading: boolean;
+// let album: number | string;
+// let re: RegExp = /\w+/g
 
-let a = 5
-let b  = '2'
-let c =12
-console.log(a / b);
+// album = 10
+// meaningOfLife = 52
+// isLoading = false
+// myName = "42"
+
+// const sum = (a: number, b: string) => {
+//     return a + b
+// }
+
+// let tuple: [string, number, boolean] = ['tamim', 42, true]
+// let mixed = ['ss', 15, false]
+
+// // obj
+// let myObj: object
+// const example = {
+//     prop1: 'tamim',
+//     prop2: true
+// }
+
+// type person = {
+//     name: string,
+//     age: number,
+//     isMarried: boolean
+// }
+
+// const sakib: person = {
+//     name: "hassan",
+//     age: 5,
+//     isMarried: true
+// }
+
+
+
+// type aliases
+// type Guitarist {
+// name: string,
+// isActive: boolean,
+// album: (string | number)[]
+// }
+type stringOrNumber = string | number
+type stringNumberArray = (string | number)[]
+
+
+interface Guitarist {
+    name: string,
+    isActive: boolean,
+    album: (string | number)[]
+}
+
+type userId = stringOrNumber
+
+type userName = 'dave' | 'tamim' | 'hassan'
+
+let newUser: userName = 'dave'
+
+const sum = (a: number, b: number): number => {
+    return a + b
+}
+
+const logMeg = (meg: any): void => {
+    console.log(meg);
+
+}
+
+logMeg('hello !')
+logMeg(sum(4, 3))
+logMeg(sum(10, 10))
+
+// type mathFunction = (a: number, b: number) => number
+interface mathFunction { (a: number, b: number): number }
+
+let multi: mathFunction = (a, b) => {
+    return a * b
+}
+
+// optional parameters
+
+const addAll = (a: number, b: number, c: number = 0): number => {
+    // if (typeof (c) !== 'undefined') {
+    //     return a + b + c
+    // }
+    return a + b + c
+}
+logMeg(addAll(2, 2, 5))
+logMeg(addAll(2, 2))
+
+// rest parameters
+
+const total = (...nums: number[]): number => {
+    return nums.reduce((prev, curr) => prev + curr);
+}
+
+logMeg(total(1,2,3,5,74))
